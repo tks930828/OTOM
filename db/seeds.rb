f1eb9c74dec88353cc51efdf58c8b1e33d4c3301
tks930828 <196395209+tks930828@users.noreply.github.com>
2025-06-14 15:57:47 +0900

    カテゴリー詳細のリンクを追加・seedの反映

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,17 @@
 #   end
 
 categories = [
-  "文芸",
-  "医学",
-  "工学",
-  "法律",
-  "ビジネス",
-  "語学",
-  "資格",
+  { name: "文芸", description: "小説などの書籍のカテゴリーです" },
+  { name: "医学", description: "医学に関する書籍のカテゴリーです" },
+  { name: "工学", description: "工学に関する書籍のカテゴリーです" },
+  { name: "法律", description: "法律関連の書籍のカテゴリーです" },
+  { name: "ビジネス", description: "ビジネス書のカテゴリーです" },
+  { name: "語学", description: "語学学習に関するカテゴリーです" },
+  { name: "資格", description: "資格試験対策の書籍カテゴリーです" },
 ]
 
-categories.each do |category|
-  Category.find_or_create_by(name: category)
+categories.each do |category_attrs|
+  category = Category.find_or_initialize_by(name: category_attrs[:name])
+  category.description = category_attrs[:description]
+  category.save!
 end
