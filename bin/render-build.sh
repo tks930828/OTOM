a67d524a121f43f3d94a8f1c791c98ce56caa55f
tks930828 <196395209+tks930828@users.noreply.github.com>
2025-06-22 11:22:47 +0900

    Dockerfilrの記述

---
+++ bin/render-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+apt-get update && apt-get install -y libpq-dev
+bundle install
+bundle exec rails assets:precompile
+bundle exec rails db:migrate
+bundle exec rails db:seed
